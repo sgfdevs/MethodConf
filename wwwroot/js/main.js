f4417060d9fdc6ddf76eda104282d1a1047a2bd3
Myke Bates <contact@mykebates.com>
2019-12-14 18:02:26 -0600

    footer, start of form submission logic, and some general cleanup

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,5 +1,13 @@
-$(function() {
-    $('.something').on('click', function () {
-        console.log("Horsin' Around");
-    });
+document.getElementById('subscribe').addEventListener('submit', function (e) {
+    var name = document.getElementById('name'),
+        email = document.getElementById('email');
+    
+    swal('Excellent!', 'Thanks, ' + name.value + '. Method Conf news will be headed your way soon.', 'success');
+    
+    name.value = '';
+    email.value = '';
+    
+    // Post to Zapier
+    
+    e.preventDefault();
 });
